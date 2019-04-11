1f700a3491b6050ad0238f63f7173afe448b29ea
Kati Frantz <bahdcoder@gmail.com>
2019-04-11 21:31:15 +0100

    lesson-1-project-setup

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+
+const app = new Vue({
+    el: '#app'
+});
---
+++ resources/js/bootstrap.js
@@ -0,0 +1,19 @@
+
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
