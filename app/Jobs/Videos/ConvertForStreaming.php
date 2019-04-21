947e7182f39114689fa992d5ba8f511c14c0850c
Kati Frantz <bahdcoder@gmail.com>
2019-04-21 21:21:52 +0100

    lesson-19-video-conversion-with-queue-jobs

--- app/Jobs/Videos/ConvertForStreaming.php
+++ app/Jobs/Videos/ConvertForStreaming.php
@@ -2,7 +2,9 @@
 
 namespace Laratube\Jobs\Videos;
 
+use FFMpeg;
 use Laratube\Video;
+use FFMpeg\Format\Video\X264;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
@@ -32,6 +34,16 @@ class ConvertForStreaming implements ShouldQueue
      */
     public function handle()
     {
-        echo 'converted';
+       $low = (new X264('aac'))->setKiloBitrate(100);
+       $mid = (new X264('aac'))->setKiloBitrate(250);
+       $high = (new X264('aac'))->setKiloBitrate(500);
+
+       FFMpeg::fromDisk('local')
+            ->open($this->video->path)
+            ->exportForHLS()
+            ->addFormat($low)
+            ->addFormat($mid)
+            ->addFormat($high)
+            ->save("public/videos/{$this->video->id}/{$this->video->id}.m3u8");
     }
 }
